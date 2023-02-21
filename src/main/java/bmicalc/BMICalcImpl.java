5cf6add2ca0524a89364624f55c0d88955a58f74
marioperales10 <mapedi@uma.es>
2023-02-21 22:00:41 +0100

    Ultim modificación

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -1,17 +1,55 @@
-package bmicalc;
 
-public class BMICalcImpl implements BMICalc {
-
+public class BMICalcImpl  implements BMICalc {
+	
 	public double bmi(double mass, double height) {
-		return 0.0;
+		
+		double imc = 0;
+		
+		if ((mass > 0 && mass < 400) && (height > 0 && height < 3)) {
+			imc = mass / (height * height);
+			
+		} else { 
+			System.out.println("Los datos no son válidos, ERROR.");
+		}
+	
+		return imc;
 	}
-
+	
 	public String category(double bmi) {
-		return "";
+		String result;
+		
+		if (bmi < 18.5 ) {
+			result = "UNDERWEIGHT";
+		}
+		else if (bmi >= 18.5  && bmi <= 24.9) {
+			result = "NORMAL";
+		}	
+		else if (bmi <= 25.0 && bmi <= 29.0) {
+			result = "OVERWEIGHT";
+		}
+		
+		else {
+			result = "OBESE";
+		}
+		return result;
+		
 	}
-
-	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+	
+	public boolean abdominalObesity(double waistCircumference, char gender){
+		
+		boolean esObeso = false;
+		
+		if (gender == 'M' && waistCircumference >= 90.0) {
+			esObeso = true;
+		}
+		
+		if (gender == 'F' && waistCircumference >= 80.0) {
+			esObeso = true;
+		}
+		
+		return esObeso; 
+		
 	}
-
+	
+	
 }
